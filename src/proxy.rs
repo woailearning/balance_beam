95aa04c647ebd7a3a18e7c98d7e28ea07b7e74aa
x <1225690373@qq.com>
2024-07-13 20:19:37 +0800

    running on proxy.rs

--- src/proxy.rs
+++ src/proxy.rs
@@ -3,6 +3,7 @@ use crate::response;
 
 use std::{collections::HashMap, sync::Arc, time::Duration, io::{Error, ErrorKind}};
 use hyper::client;
+use rand::Rng;
 use rand::SeedableRng;
 use tokio::{
     net::{TcpListener, TcpStream},
@@ -74,7 +75,7 @@ pub(crate) async fn active_health_check(state: &ProxyState) {
 
             // Open a connection to a destination server
             match TcpStream::connect(upstream_ip).await {
-                Ok( mut conn ) => {
+                Ok(mut conn) => {
                     // Write to stream and read from stream
                     if let Err(error) = request::write_to_stream(&request, &mut conn).await { // Request::Error
                         log::error!(
@@ -82,7 +83,7 @@ pub(crate) async fn active_health_check(state: &ProxyState) {
                             upstream_ip,
                             error
                         );
-                        return ;
+                        return;
                     }
 
                     let response = match response::read_from_stream(&mut conn, &request.method()).await {
@@ -117,6 +118,46 @@ pub(crate) async fn active_health_check(state: &ProxyState) {
     }
 }
 
+
+/// 
+/// 
+/// # Brief
+///
+/// # Param
+///
+/// # Return
+/// 
+async fn connect_to_upstream(state: &ProxyState) -> Result<TcpStream, std::io::Error> {
+    let mut rng = rand::rngs::StdRng::from_entropy();
+
+    loop {
+        let live_upstream_addresses = state.live_upstream_addresses.read().await;
+        let upstream_idx = rng.gen_range(0..live_upstream_addresses.len());
+        let upstream_ip = &live_upstream_addresses.get(upstream_idx).unwrap().clone();
+
+        drop(live_upstream_addresses); // release; read lock
+
+        match TcpStream::connect(upstream_ip).await {
+            Ok(stream) => return Ok(stream),
+            Err(err) => {
+                log::error!("Failed to connect to upstream {}: {}", upstream_ip, err);
+
+                let mut live_upstream_addresses = state.live_upstream_addresses.write().await;
+                live_upstream_addresses.swap_remove(upstream_idx); // remove the address from live_upstream_address.
+
+                // All upstreams are dead, return Err
+                if live_upstream_addresses.len() == 0 {
+                    log::error!("All upstreams are dead");
+                    return Err(Error::new(ErrorKind::Other, "All upstreams are dead"));
+                }
+            },
+        }
+    }
+}
+
+async fn handle_connection(client_conn: TcpStream, state: &ProxyState) {
+}
+
 ///
 /// # Brief
 /// 
@@ -131,18 +172,18 @@ async fn send_response(client_conn: &mut TcpStream, response: &http::Response<Ve
         client_ip,
         response::format_response_line(&response)
     );
-    if let Err(error) = response::write_to_stream(response, client_conn).await {
-        log::warn!("All upstream are dead");
+    if let Err(err) = response::write_to_stream(response, client_conn).await {
+        log::warn!("Failed to send response to the client {}", err);
         return;
     }
 }
 
 ///
-/// 
-/// # Brief
 /// This asynchronous function checks if the rate limit for a client IP has been exceded.
 /// If he client exceded the maximum allowed requests per minute, an HTTP error response
 /// is sent back to the client and an error is retuned. Otherwise, the function returns Ok(()).
+/// 
+/// # Brief
 /// 
 /// # Param
 /// - `state`: A reference to the 'ProxyState' which contains the rate limiting counter and 
@@ -155,7 +196,7 @@ async fn send_response(client_conn: &mut TcpStream, response: &http::Response<Ve
 /// 
 async fn check_rate(state: &ProxyState, client_conn: &mut TcpStream) -> Result<(), std::io::Error> {
     let client_ip = client_conn.peer_addr().unwrap().ip().to_string();
-    let rate_limiting_counter = state.rate_limiting_counter.clone().lock_owned().await;
+    let mut rate_limiting_counter = state.rate_limiting_counter.clone().lock_owned().await;
     // need to wait for get owner for this variable and process it.
     let cnt = rate_limiting_counter.entry(client_ip).or_insert(0);
 
@@ -172,14 +213,17 @@ async fn check_rate(state: &ProxyState, client_conn: &mut TcpStream) -> Result<(
     Ok(())
 }
 
-/// # Brief
 /// This asynchronous function continuously clears the rate limiting counter at a specified
 /// interval. It runs an infinite loop that sleeps for the given interval and then clears 
 /// the rate limiting counter.
+/// 
+/// # Brief
 ///
 /// # Param
 /// - `state`: A reference to the `ProxyState` which contains the rates limiting counter.
 /// - `clear_interval`: The interval in seconds at which the rate limiting counter should be cleared.
+/// 
+/// # Return
 /// 
 async fn rate_limiting_counter_clearer(state: &ProxyState, clear_interval: u64) {
     loop {
@@ -188,93 +232,4 @@ async fn rate_limiting_counter_clearer(state: &ProxyState, clear_interval: u64) 
         let mut rate_limiting_counter = state.rate_limiting_counter.clone().lock_owned().await;
         rate_limiting_counter.clear();
     }
-}
-
-
-/// # Brief
-///
-/// # Param
-///
-/// # Return
-/// 
-///
-/// 
-/*
-pub(crate) async fn active_health_check(state: &ProxyState) {
-    loop {
-        sleep(
-            Duration::from_secs(
-                state.active_health_check_interval.try_into().unwrap(),
-            )
-        ).await;
-
-        let mut live_upstream_addresses = state.live_upstream_addresses.write().await;
-        live_upstream_addresses.clear();
-
-        // send a request to each upstream
-        // If a failed upstream return HTTP 200, put it back in the roation of upstream servers
-        // If an online upstream returns a not-200 status code, mark that server as failed
-        for upstream_ip in &state.upstream_addresses {
-            let request: http::Request<Vec<u8>> = http::Request::builder()
-                .method(http::Method::GET)
-                .uri(&state.active_health_check_path)
-                .header("Host", upstream_ip)
-                .body(Vec::<u8>::new())
-            .unwrap();
-
-            // Open a connection to a destination server
-            match TcpStream::connect(upstream_ip).await {
-                Ok( mut conn ) => {
-                    // Write to stream and read from stream
-                    if let Err(error) = request::write_to_stream(&request, &mut conn).await {
-                        log::error!(
-                            "Failed to send request to upstream {}: {}",
-                            upstream_ip,
-                            error
-                        );
-                        return ;
-                    }
-
-                    let response = match response::read_from_stream(&mut conn, &request.method()).await {
-                        Ok(response) => response,
-                        Err(error) => {
-                            log::error!("Error reading response from server: {:?}", error);
-                            return;
-                        }
-                    };
-
-                    // Handle the statusCode of response
-                    match response.staus().as_u16() {
-                        200 => {
-                            live_upstream_addresses.push(upstream_ip.clone());
-                        },
-                        status @ _ => {
-                            log::error!(
-                                "upstream server {} is not working: {}",
-                                upstream_ip,
-                                status
-                            );
-                            return;
-                        }
-                    };
-                },
-                Err(err) => {
-                    log::error!("Failed to connect to upstream {}: {}", upstream_ip, err);
-                    return;
-                }
-            }
-        }
-    }
-}
-*/
-
-
-/// # Brief
-///
-/// # Param
-///
-/// # Return
-/// 
-async fn connect_to_upstream() -> Result<> {
-
 }
