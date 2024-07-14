52cf5f17a398155c5a36f014a95164158c8d7311
x <1225690373@qq.com>
2024-07-14 16:38:18 +0800

    complete logic core of proxy

--- src/proxy.rs
+++ src/proxy.rs
@@ -2,7 +2,6 @@ use crate::request;
 use crate::response;
 
 use std::{collections::HashMap, sync::Arc, time::Duration, io::{Error, ErrorKind}};
-use hyper::client;
 use rand::Rng;
 use rand::SeedableRng;
 use tokio::{
@@ -255,4 +254,92 @@ async fn handle_connection(mut client_conn: TcpStream, state: &ProxyState) {
     log::info!("Connection received from {}", client_ip);
 
     // Open a connection to a random destination server.
+    let mut upstream_conn = match connect_to_upstream(state).await {
+        Ok(stream) => stream,
+        Err(_error) => {
+            let response = response::make_http_error(http::StatusCode::BAD_GATEWAY);
+            send_response(&mut client_conn, &response).await;
+            return;
+        },
+    };
+    let upstream_ip = upstream_conn.peer_addr().unwrap().ip().to_string();
+
+    // The client may now send us one or more requests. Keep trying to read requests until 
+    // the client hangs up or we get an error.
+    loop {
+        // read a request from the client.
+        let mut request = match request::read_from_stream(&mut client_conn).await {
+            Ok(request) => request,
+            Err(request::Error::IncompleteRequest(0)) => {
+                log::debug!("Client finished sending requests. Shutting down connection");
+                return;
+            },
+            Err(request::Error::ConnectionError(io_err)) => {
+                // handle I/O error in reading from the client
+                log::info!("Error reading request from client stream {}", io_err);
+                return;
+            },
+            Err(error) => {
+                log::debug!( "Error parsing request: {:?}", error);
+                let response = response::make_http_error(match error {
+                    request::Error::IncompleteRequest(_)
+                    | request::Error::MalformaedRequest(_)
+                    | request::Error::InvaildContentLength
+                    | request::Error::ContentLengthMismatch => http::StatusCode::BAD_REQUEST,
+
+                    request::Error::RequestBodyTooLarge => http::StatusCode::PAYLOAD_TOO_LARGE,
+
+                    request::Error::ConnectionError(_) => http::StatusCode::SERVICE_UNAVAILABLE,
+                });
+                send_response(&mut client_conn, &response).await;
+                continue;
+            },
+        };
+        log::info!(
+            "{} -> {}: {}",
+            client_ip,
+            upstream_ip,
+            request::format_request_line(&request)
+        );
+
+        // rate limiting
+        if state.max_requests_per_minutes > 0 {
+            if let Err(_error) = check_rate(&state, &mut client_conn).await {
+                log::error!("{} rate limiting", &client_ip);
+                continue;
+            }
+        }
+
+        // Add X-Forwarded-For header so that the upstrram server knows the client's IP address.
+        // (we're the ones connecting directly to the upstream server, so without this header, the
+        // upstream server will only know our IP, not the client's)
+        request::extend_header_value(&mut request, "x-forwarded-for", &client_ip);
+
+        // Forward the request to the server
+        if let Err(err) = request::write_to_stream(&request, &mut upstream_conn).await {
+            log::error!(
+                "Filed to send request to the upstream {}: {}",
+                upstream_ip,
+                err
+            );
+            let response = response::make_http_error(http::StatusCode::BAD_GATEWAY);
+            send_response(&mut client_conn, &response).await;
+            return;
+        }
+        log::debug!("Forwarded request to server");
+
+        let response = match response::read_from_stream(&mut upstream_conn, request.method()).await {
+            Ok(response) => response,
+            Err(error) => {
+                log::error!("Error reading response from server: {:?}", error);
+                let response = response::make_http_error(http::StatusCode::BAD_GATEWAY);
+                send_response(&mut client_conn, &response).await;
+                return;
+            },
+        };
+
+        // Forward the response to the client.
+        send_response(&mut client_conn, &response).await;
+        log::debug!("Forwarded response to client");
+    }
 }
